6b28f74035e090a34ae08f2676283e7dfb7d855d
Sebastian Lopez Buritica <selobu@gmail.com>
2022-09-14 08:05:30 -0500

    Add email validation add user output model

--- app/db.py
+++ app/db.py
@@ -1,10 +1,23 @@
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class User(BaseModel):
         nombres: str
         apellidos: str
+        correo: EmailStr
         password: str='123456'
+        cedula: str
+        departamento: str
+        municipio: str
+        direccion: str
+        activo: bool = True
+        pertenecealgrupo: bool = False
+
+# defined to hide the password
+class UserOut(BaseModel):
+        nombres: str
+        apellidos: str
+        correo: EmailStr
         cedula: str
         departamento: str
         municipio: str
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
-from .db import User
+from .db import User, UserOut
 app = FastAPI()
 
 
@@ -9,18 +9,20 @@ app = FastAPI()
 def read_root():
     return {"docs": "/docs"}
 
-@app.get("/User/{user_id}", response_model=User)
+@app.get("/User/{user_id}", response_model=UserOut)
 async def read_user(user_id:int, q: Union[str, None]=None):
-    usr = User(nombres='', apellidos='',cedula='',
-              departamento='',municipio='', direccion='')
-    return usr
+    user = User(nombres='', apellidos='',cedula='',
+            correo='noreply.noreply@gestionhseq.com',
+            departamento='',municipio='', direccion='')
+    return user
 
-@app.post("/User/", response_model=User)
+@app.post("/User/", response_model=UserOut)
 async def registrar_user(user: User):
     return user
 
-@app.put("/User/{user_id}", response_model=User)
+@app.put("/User/{user_id}", response_model=UserOut)
 async def update_user(user_id:int, user: User):
     usr = User(nombres='', apellidos='',cedula='',
-              departamento='',municipio='', direccion='')
+            correo='noreply.noreply@gestionhseq.com',
+            departamento='',municipio='', direccion='')
     return usr
