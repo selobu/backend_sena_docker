17d3f4bfc73d12b11080e2d5d64cd282c48d16e5
Sebastian Lopez Buritica <selobu@gmail.com>
2022-09-14 08:58:52 -0500

    parametros para paginar

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from typing import Union
 
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status,\
+    Depends
 
 from .db import User, UserOut
 app = FastAPI()
+
+
+async def paginate_parameters(
+    q: Union[str, None] = None, skip: int = 0, limit: int = 20
+):
+    return {"q": q, "skip": skip, "limit": limit}
 
 
 @app.get("/")
 def read_root():
     return {"docs": "/docs"}
 
+
+@app.get("/User/")
+async def read_all_user(commons: dict = Depends(paginate_parameters)):
+    return commons
+    
 @app.get("/User/{user_id}", response_model=UserOut)
 async def read_user(user_id:int, q: Union[str, None]=None):
     user = User(nombres='', apellidos='',cedula='',
